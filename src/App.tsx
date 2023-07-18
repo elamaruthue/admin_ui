e0cab7e0b492bfb78752663a6322f4cd81d2e202
maruthu <elamaruthue@gmail.com>
2023-07-19 00:12:30 +0530

    loginsetup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SignInSide from './auths/sign';
+
+function App() {
+  return (
+    <div className="App">
+      <SignInSide />
+    </div>
+  );
+}
+
+export default App;
